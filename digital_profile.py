fad70b05acd9a2a144cd5759cc0258a907ed5391
415-33-Siriyakorn <56958@yupparaj.ac.th>
2026-06-25 09:11:35 +0700

    digital_profile.py

---
+++ digital_profile.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+# 1. หัวข้อใหญ่: แนะนำตัวหลักและชื่อเจ้าของเว็บ
+st.title("😪 My Digital Profile")
+
+# 2. หัวข้อย่อยที่ 1: แนะนำตัว
+st.header("🎵 แนะนำตัว")
+st.writer("นางสาวสิริยากร กองอินทร์")
+st.writer()
+st.writer()
+
+# 3. หัวข้อย่อยที่ 2: เป้าหมายและอนาคต
+st.header()
+st.writer()
+st.writer()
+st.writer()
