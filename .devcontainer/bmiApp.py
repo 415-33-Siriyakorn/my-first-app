559f5088c5cd828b817ce12d3307b3f2d5741baa
415-33-Siriyakorn <56958@yupparaj.ac.th>
2026-09-03 09:06:19 +0700

    Create bmiApp.py

---
+++ .devcontainer/bmiApp.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+#ส่วนที่ 1 หัวข้อหน้าเว็บ ( Title สีแดง )
+st.markdown("# :red [🏋️คำนวนดัชนีมวลกาย BMI]")
+st.write
+
+#ส่วนที่ 2 สร้างช่องรับค่าน้ำหนัก และ ส่วนสูง
+
+weight = st.number_input
+heigt_cm = st.number_imput
